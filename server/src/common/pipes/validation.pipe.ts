6551a3a3731bec41505cba92722615b59461a1e5
Keith McKiernan <keithmckiernan01@gmail.com>
2018-05-12 23:07:54 +0100

    add @todo message on validation pipe

--- server/src/common/pipes/validation.pipe.ts
+++ server/src/common/pipes/validation.pipe.ts
@@ -4,5 +4,6 @@ import { Pipe, PipeTransform, ArgumentMetadata } from '@nestjs/common';
 export class ValidationPipe implements PipeTransform<any> {
   public async transform(value, metadata: ArgumentMetadata) {
     const { metatype } = metadata;
+    // @todo finish validation pipe
   }
 }
